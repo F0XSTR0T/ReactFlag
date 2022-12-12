ebba479ed7630484b7b86b88020b8a790e21b65b
Julien2L <julien.lehoussine-labatut@epitech.eu>
2022-12-12 11:49:34 +0100

    add blog section w/ crud

--- redflag/src/App.js
+++ redflag/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
+import Blog from "./pages/Blog";
 import Home from "./pages/Home";
+import Page404 from "./pages/Page404";
+
 
 const App = () => {
   return (
@@ -10,6 +13,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
     </div>
